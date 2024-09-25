40c823774cd4e02d8f6298de0b58571646863e35
Your Name <you@example.com>
2024-09-25 11:30:27 +0500

    Integrated melo tts

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from openvoicechat.tts.tts_xtts import Mouth_xtts
-from openvoicechat.llm.llm_ollama import Chatbot_ollama
+#from openvoicechat.tts.tts_xtts import Mouth_xtts
+# from openvoicechat.tts.tts_hf import Mouth_hf
+# from openvoicechat.tts.tts_hf import Mouth_hf
+from openvoicechat.tts.tts_melo import Mouth_melo
+# from openvoicechat.tts.tts_parler import Mouth_parler
+from openvoicechat.llm.llm_gpt import Chatbot_gpt
+#from openvoicechat.llm.llm_llama import Chatbot_llama
+# from openvoicechat.llm.llm_hf import Chatbot
 from openvoicechat.stt.stt_hf import Ear_hf
 from openvoicechat.utils import run_chat
 from openvoicechat.llm.prompts import llama_sales
@@ -14,7 +20,7 @@ if __name__ == "__main__":
     if torch.backends.mps.is_available():
         device = "mps"
     elif torch.cuda.is_available():
-        device = "cuda"
+        device = "cuda:0"
     else:
         device = "cpu"
 
@@ -27,9 +33,12 @@ if __name__ == "__main__":
         listen_interruptions=False,
     )
 
-    chatbot = Chatbot_ollama(sys_prompt=llama_sales, model="qwen2:0.5b")
+    # chatbot = Chatbot_ollama(sys_prompt=llama_sales, model="qwen2:0.5b")
+    chatbot = Chatbot_gpt(sys_prompt=llama_sales, Model="gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
+    #chatbot = Chatbot_llama(model_path="models/qwen2.5-0.5b-instruct-q8_0.gguf")
 
-    mouth = Mouth_xtts(device=device)
+    mouth = Mouth_melo(device=device)
+    #mouth = Mouth_xtts(device=device)
 
     run_chat(
         mouth, ear, chatbot, verbose=True, stopping_criteria=lambda x: "[END]" in x
